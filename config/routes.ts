b73fc0a4376f6f864524691f8fcf3ac5f88524d4
Cool <990299103@qq.com>
2024-08-22 19:32:48 +0800

    fix:"完善接口查询功能的bug"

--- config/routes.ts
+++ config/routes.ts
@@ -4,7 +4,7 @@
     layout: false,
     routes: [{name: '登录', path: '/user/login', component: './User/Login'}],
   },
-  {path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome'},
+  // {path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome'},
   {
     path: '/admin',
     name: '管理页',
@@ -15,7 +15,7 @@
       {path: '/admin/sub-page', name: '二级管理页', component: './Admin'},
     ],
   },
-  {name: '查询表格', icon: 'table', path: '/list', component: './TableList'},
-  {path: '/', redirect: '/welcome'},
+  {name: '查询表格', icon: 'table', path: '/list', component: './InterfaceInfo'},
+  // {path: '/', redirect: '/welcome'},
   {path: '*', layout: false, component: './404'},
 ];
