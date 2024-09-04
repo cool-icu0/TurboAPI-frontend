930faa95d5e982cdee0e5e8762fe696037b0e643
Cool <990299103@qq.com>
2024-09-04 23:36:51 +0800

    feat:"实现数据分析功能"

--- config/routes.ts
+++ config/routes.ts
@@ -9,6 +9,8 @@
   { path: '/admin', name: '管理页', icon: 'crown', access: 'canAdmin',
     routes: [
       { name: '接口管理',icon:'table',path: '/admin/interface_info', component: './Admin/InterfaceInfo'},
+
+      { name:'接口分析',icon:'analytics',path: '/admin/interface_analysis', component: './Admin/InterfaceAnalysis'},
     ],
   },
   {path: '*', layout: false, component: './404'},
